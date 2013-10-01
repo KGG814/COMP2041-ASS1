185d38f95d4e23bf4250bb65d904e909a5b5098a
Kalana Gamlath <kalana.gamlath1@gmail.com>
2013-10-01 12:45:18 +1000

    Started at 11:15
    Did subset 0 and 1. Converted previous code to subroutines, added some
    comments. Also did variable interpolation. Fixed minor bugs.

--- perl2python.pl
+++ perl2python.pl
@@ -1,30 +1,73 @@
 #!/usr/bin/perl
 use warnings;
+use strict;
+our @lines = ();
+while (my $line = <>) {
+	chomp $line;
+	$line = &pythonInitialise($line);
+	$line = &removeSemicolons($line);
+	$line = &removeNewlinePrint($line);
+	$line = &printSimple($line);
+	#$line = &interpolate($line);
+	$line = &convertVars($line);
+	
+	push @lines, $line;	
+}
 
-while ($line = <>) {
-	if ($line =~ m/^#!/ and $. == 1) {
-	
-		# translate #! line 
-		
-		print "#!/usr/bin/python2.7 -u\n";
-	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-	
-		# Blank & comment lines can be passed unchanged
-		print $line;
-	} else {
-		# Strip commas
-		if ($line =~ m/^(.*);/) {
-			$line = $1;
-		}
-		# Python's print adds a new-line character by default
-		# so we need to delete it from the Perl print statement
-		if ($line =~ m/^\s*print\s*"(.*)\\n"[\s]*$/) {	
-			$line = "print '$1'";
-			while ($line =~ /'.*\$[a-zA-Z0-9]+.*'[\s]*/) {	
-				$line =~ s/(\$[a-zA-Z0-9]+)/\%s/ or die;	
-				$line = "$line, $1";
-			}
-		}
-		print "$line\n";
+print "$_\n" for @lines;
+# Set first line to use python compiler
+sub pythonInitialise {
+	if ($_[0] =~ m/^#!/ and $. == 1) {		
+		return "#!/usr/bin/python2.7 -u";		
 	}
+	return $_[0];
+}
+
+# Strip semicolons
+sub removeSemicolons {
+	if ($_[0] =~ m/^(.*);/) {
+			return $1;
+	}
+	return $_[0];
+}
+
+# Python's print adds a new-line character by default
+# so we need to delete it from the Perl print statement
+sub removeNewlinePrint {
+	if ($_[0] =~ m/^print\s*\"([^\"]*)\\n\"[\s]*$/) {	
+			return "print '$1'";
+	}
+	return $_[0];
+}
+
+# print a single variable without interpolating
+sub printSimple {
+	if ($_[0] =~ /^print \'(\$[^\"\s\$]+[\s\"]*)+\'/) {
+		$_[0] =~ /^print \"(.*)\"/;
+		my @varList = grep{/\S/} split(/\$/, $1);
+		my $vars = join(",", @varList);
+		$_[0] = "print $vars";
+	}
+	return $_[0];
+}
+
+# Variable interpolation, experimental
+sub interpolate {
+	while ($_[0] =~ /(.*\')([^\$]*)(\$[^\s\\]+)(.*\')/) {	
+		my $sub = $3;
+		$_[0] = "$1\%s$2$4 \% $3" ;
+	}
+   return $_[0];
+}
+
+#convert variables
+sub convertVars {
+	$_[0] =~ s/\$//;
+	return $_[0];
+}
+
+sub testPrint {
+	print "----------------\n\n";
+	print "$_[0]\n";
+	print "----------------\n\n";
 }
---
+++ tests/answer0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python2.7 -u
+
+answer = 42
+print '%s' % answer
+
